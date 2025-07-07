142af3a21cce884686ee38e89ad75e350f86d45f
TutCode222 <tuddycarraway@gmail.com>
2025-07-06 19:24:09 -0500

    Add script to trigger API call and results

---
+++ script.js
@@ -0,0 +1,3 @@
+// 1️⃣ Select DOM elements
+const searchButton = document.getElementById('searchBtn');  // Button to trigger the API call
+const bookContainer = document.getElementById('bookResults');  // Section where books will be displayed
